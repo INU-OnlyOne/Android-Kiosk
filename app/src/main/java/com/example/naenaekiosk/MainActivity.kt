3ecedcceb9575fbe88b2cee31fecd9d910e51e88
mummhy0811 <mummhy0811@inu.ac.kr>
2023-03-11 18:37:16 +0900

    feat: 회원가입 아이디 비번 확인 로직

--- app/src/main/java/com/example/naenaekiosk/MainActivity.kt
+++ app/src/main/java/com/example/naenaekiosk/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.naenaekiosk
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.naenaekiosk.databinding.ActivityMainBinding
+import com.example.naenaekiosk.databinding.ActivityManageWaitingBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding =  ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.signUp!!.setOnClickListener {
+            val intent=Intent(this, SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.button4.setOnClickListener{
+
+        }
     }
 }
